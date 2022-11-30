edbbeaf495ca8f3729adb4e467701608ac335580
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-11-29 21:58:10 -0800

    added all arrays of password requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-
-var upperCaseChoice = confirm(
-  "Would you want to include an uppercase letter in your password?"
-);
-
-var lowerCaseChoice = confirm(
-  "Would you want to include a lowercase letter in your password?"
-);
-
-var numberChoice = confirm(
-  "Would you want to include a number in your password?"
-);
-
-var specialCharacterChoice = confirm(
-  "Would you want to include a special character in your password?"
-);
-
-console.log(upperCaseChoice);
-console.log(lowerCaseChoice);
-console.log(numberChoice);
-console.log(specialCharacterChoice);
-
-if (
-  !upperCaseChoice && !lowerCaseChoice && !specialCharacterChoice && !numberChoice
-) {
-  alert("You must meet all character types");
-  return generatePassword
-}
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+let upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharacterArray = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", "|", "/", "'", ":", ";", "<", ",", ">", ".", "?"];
+//arrays of characters
